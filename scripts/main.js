c419f71e7c3cfb0ff4d097363d00343106c7e989
Adam Filandr <adam.filandr@datamole.cz>
2022-01-02 01:55:24 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,24 @@
 const myHeading = document.querySelector('h1');
 
-document.getElementById('myButton').addEventListener('click', e => {
+document.getElementById('myButton1').addEventListener('click', e => {
     navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
         optionalServices: ['battery_service']
     })
         .then(device => {
-        //Paired
+            //Paired
         })
         .catch(error => {
-        //Handle Errors
+            //Handle Errors
         });
 });
 
-myHeading.textContent = 'Hello world!';
+document.getElementById('myButton2').addEventListener('click', e => {
+    navigator.usb.getDevices()
+        .then(devices => {
+            myHeading.textContent = "Total devices: " + devices.length;
+            devices.forEach(device => {
+                console.log("Product name: " + device.productName + ", serial number " + device.serialNumber);
+            });
+        });
+});
